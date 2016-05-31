b562def9b77aba11b31e9980ec2b7a9824dc6cc1
Tobias Lorenz <tobias.lorenz@bitmazk.com>
2016-05-31 07:12:09 +0200

    Replaced locale currency with intcomma and floatformat

--- account_keeping/templatetags/account_keeping_tags.py
+++ account_keeping/templatetags/account_keeping_tags.py
@@ -1,15 +1,14 @@
-import locale
-
 from django import template
+from django.contrib.humanize.templatetags.humanize import intcomma
+from django.template.defaultfilters import floatformat
 from django.utils.timezone import datetime
 
-locale.setlocale(locale.LC_ALL, 'en_GB.UTF-8')
 register = template.Library()
 
 
 @register.filter()
 def currency(value):
-    return locale.currency(value, symbol=False, grouping=True)
+    return intcomma(floatformat(value, '-2'))
 
 
 @register.assignment_tag()
